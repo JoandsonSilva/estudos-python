c6a7868f6ea4b367131c0975fc49f71e0b4e1ca5
JoandsonOliveira <91487025+JoandsonSilva@users.noreply.github.com>
2025-02-05 13:06:02 -0300

    Python

---
+++ Python/Curso de Python - Santander/02_Input.py
@@ -0,0 +1,4 @@
+nome = input("Digite seu nome: ")
+idade = input ("Qual é a sua idade?")   
+
+print("Seu nome é: " + nome + " e você tem " + idade  + "anos")
---
+++ Python/Curso de Python - Santander/03_função_if_else.py
@@ -0,0 +1,13 @@
+idade = int (input("Insira sua idade "))
+
+if idade >=18:
+    print ("Você é maior de idade")
+else:
+    print("Você é menor de idade.")
+
+
+
+nome ="Joandson"
+idade = 25
+
+print( f"Olá, meu nome é {nome} e tenho {idade} anos")
