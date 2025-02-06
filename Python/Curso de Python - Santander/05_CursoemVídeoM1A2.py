e786dfb6f2f90fcd6eeb011313d32cf561cab01a
JoandsonOliveira <91487025+JoandsonSilva@users.noreply.github.com>
2025-02-06 13:39:40 -0300

    Atualization

---
+++ Python/Curso de Python - Santander/05_CursoemVídeoM1A2.py
@@ -0,0 +1,5 @@
+programador = "1989 - Guido Van Hossum Amsterdam Holanda"
+
+python = "Linguagem de propósito geral / multiplataforma / zen of python / Bonito é melhor que feio / Explicito é melhor do que implícito / Simples é o melhor que complexo / Complexo é melhor que complicado / Linear é melhor do que aninhado /Esparso é melhor que dento / Legibilidade conta / Casos especiais não são especiais o bastante para quebrar as regras. / Ainda que praticipade vença a pureza . / Erros nunca devem passar silencionamente./ A menos que sejam explicitamente silenciados. / Diante da ambiguidade, recuse a tentação de adivinhar. / Deveria haver um - e  preferencialmente só um - modo óbrio para fazer algo. / Embora esse modo possa não ser óbvio a princípio a menos que você seja holandês. / Agora é melhor do que nunca "
+
+areaAplicacao = "Inteligência Artificial / Biotecnologia / Computação 3D"
