62cb0f963289782cffba2b8e961ad67f5351ba96
JoandsonOliveira <91487025+JoandsonSilva@users.noreply.github.com>
2025-02-20 15:19:11 -0300

    py 2

--- Python/Curso de Python - Santander/08_1Aula08b.py
+++ Python/Curso de Python - Santander/08_1Aula08b.py
@@ -1,2 +1,8 @@
-n = bool(input("Digite um valor"))
-print(n)
+n = input("Digite algo " )
+
+print (n.isnumeric())
+
+
+print(n.isalpha())
+
+print(n.is)
---
+++ Python/Curso de Python - Santander/Desafios/003exercício.py
@@ -0,0 +1,8 @@
+
+
+
+n = int(input("Digite um número"))
+n2= int (input("Digite outro número "))
+
+resultado = (n+n2)
+print ("O resultado entre {} + {} é {}".format(n,n2, resultado))
