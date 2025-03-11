e79c7965e14805d047537a013653614a45fbaf23
JoandsonOliveira <91487025+JoandsonSilva@users.noreply.github.com>
2025-03-11 10:06:23 -0300

    11/03

---
+++ Python/Curso de Python - Santander/08_1Aula07b.py
@@ -0,0 +1,18 @@
+n1 = int(input('Digite um número '))
+print(n1)
+
+
+operadores aritméritocs = Soma +, Subtração- , Multiplicação*, Divisão /, potência **,  Divisão inteira //, resto da divisão %
+
+
+Operador relacional
+= atribuição
+== igual
+
+
+Ordem de procedência 
+
+1º ()
+2º ** potência - exponenciação
+3º *, / , //, %
+4º +, -
--- Python/Curso de Python - Santander/08_1Aula08b.py
+++
@@ -1,8 +0,0 @@
-n = input("Digite algo " )
-
-print (n.isnumeric())
-
-
-print(n.isalpha())
-
-print(n.is)
