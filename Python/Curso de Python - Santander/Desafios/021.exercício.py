8f3c0fd1862a039a118c3ac7c68fea1b00ff4fb6
JoandsonOliveira <91487025+JoandsonSilva@users.noreply.github.com>
2025-04-08 13:04:50 -0300

    Python

---
+++ Python/Curso de Python - Santander/Desafios/021.exercício.py
@@ -0,0 +1,7 @@
+import pygame
+import time
+pygame.init()
+pygame.mixer.music.load('C:/Users/Audiovisual/Documents/GitHub/B-sicos/Python/Curso de Python - Santander/Desafios/saulo.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+time.sleep(10)
